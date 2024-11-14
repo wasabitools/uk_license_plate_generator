b2de9ba2f5e86fade069ac44281edd7e2057c232
natasafi <natasafrag@gmail.com>
2024-11-14 21:30:13 +0000

    adds validation function

--- license_plate_generator/app/validators.py
+++ license_plate_generator/app/validators.py
@@ -2,9 +2,8 @@ import re
 from datetime import datetime
 
 def validate_memory_tag(memory_tag: str) -> bool:
-    memory_tag = memory_tag.upper()
     pattern = r"^[A-HK-PR-Y]{2}$"
-    return bool(re.match(pattern, memory_tag))
+    return bool(re.match(pattern, memory_tag.upper()))
 
 def validate_date(date: str) -> bool:
     try:
@@ -12,3 +11,7 @@ def validate_date(date: str) -> bool:
         return date_value <= datetime.now()
     except ValueError:
         return False
+    
+def validate_generated_plate(license_plate: str) -> bool:
+    plate_format = r"^[A-HK-PR-Y]{2}\d{2} \W{3}$"
+    return bool(re.match(plate_format, license_plate))
