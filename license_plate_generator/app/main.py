ac13e25ebeed443433b5cee836642d9b2774be83
natasafi <natasafrag@gmail.com>
2024-11-14 20:39:27 +0000

    adds basic main function

---
+++ license_plate_generator/app/main.py
@@ -0,0 +1,10 @@
+def main():
+    memory_tag = input("Enter two letters excluding I, Q, Z: ")
+    date = input("Enter date in dd/mm/yyyy format: ")
+
+# add memory_tag validation
+# add date validation
+# add generator function
+
+if __name__ == "__main__":
+    main()
