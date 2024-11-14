b804ab10e8d65ff2833d78f7b03825bfc64ff2d7
natasafi <natasafrag@gmail.com>
2024-11-14 21:58:34 +0000

    adds exceptions

---
+++ license_plate_generator/app/exceptions.py
@@ -0,0 +1,11 @@
+class InvalidMemoryTagError(Exception):
+    pass
+
+class InvalidDateError(Exception):
+    pass
+
+class FutureDateError(Exception):
+    pass
+
+class InvalidLicensePlateError(Exception):
+    pass
